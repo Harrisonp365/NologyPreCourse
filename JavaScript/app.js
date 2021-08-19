6b904ee9f31921166883866deb9d66a2dc9e42d9
Harrisonp365 <harrisonpowell365@gmail.com>
2021-08-19 14:34:16 +1000

    basic jump functionality introducted

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -5,11 +5,19 @@ document.addEventListener('DOMContentLoaded' , () => {
 
   let birdLeft = 220
   let birdBottom = 100
+  let birdDrop = 2 // bird will drop 2px every 20ms
 
   function startGame() {
+    birdBottom -= birdDrop
     bird.style.bottom = birdBottom + 'px'
     bird.style.left = birdLeft + 'px'
   }
 
-  startGame()
+  let limer = setInterval(startGame, 20)
+
+  function jump() {
+    birdBottom += 50 //Each jump is 50px above current position
+    bird.style.bottom = birdBottom + 'px'
+  }
+  document.addEventListener('keyup', jump) // can use any key on keyboard
 })
