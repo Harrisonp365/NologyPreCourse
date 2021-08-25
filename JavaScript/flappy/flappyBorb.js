fe7dd80295024516e2c043bb5901f9a676c80099
Harrisonp365 <harrisonpowell365@gmail.com>
2021-08-25 14:20:32 +1000

    setting everything up for flappy bird

--- JavaScript/flappy/flappyBorb.js
+++ JavaScript/flappy/flappyBorb.js
@@ -1,24 +1,3 @@
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
 
-//Images
-var bird = new Image();
-var backGround = new Image();
-var foreGround = new Image();
-var pipeNorth = new Image();
-var pipeSouth = new Image();
-
-bird.src = "Images\birdSprite.png";
-backGround.src = "Images\FBbackground.png";
-//foreGround.src = "Images\fg.png";
-pipeNorth.src = "Images\FBTubeNorth.png";
-//pipeSouth.src = "Images\pipeSouth.png";
-
-// display images
-
-function draw() {
-  ctx.drawImage(backGround, 0, 0);
-  ctx.drawImage(bird, 128, 128);
-}
-
-draw();
