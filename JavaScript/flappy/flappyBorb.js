8e33d0da511d47cb73a57bfd8e47258c5e442127
Harrisonp365 <harrisonpowell365@gmail.com>
2021-08-26 10:31:14 +1000

    most draw functions are done, need to develop a game state and continue from there

--- JavaScript/flappy/flappyBorb.js
+++ JavaScript/flappy/flappyBorb.js
@@ -39,11 +39,63 @@ const foreGround = {
 
 }
 
+//The bird has 3 images one for each animation up, middle, down
+const bird = {
+  animation : [
+    {sX: 276, sY: 112},
+    {sX: 276, sY: 139},
+    {sX: 276, sY: 164},
+    {sX: 276, sY: 139}
+  ],
+  x: 50,
+  y: 150,
+  w: 34,
+  h: 26,
+  frame: 0,
+
+  draw : function() {
+    let bird = this.animation[this.frame];
+
+    ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, this.x - this.w/2, this.y - this.h/2, this.w, this.h);
+  }
+}
+
+//Ready message
+const ready = {
+  sX: 0,
+  sY: 228,
+  w: 173,
+  h: 152,
+  x: cvs.width/2 - 173/2,
+  y: 80,
+
+  draw : function() {
+    ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+  }
+}
+
+//Game over
+const gameOver = {
+  sX: 175,
+  sY: 228,
+  w: 225,
+  h: 202,
+  x: cvs.width/2 - 225/2,
+  y: 90,
+
+  draw : function() {
+    ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+  }
+}
+
 function draw() {
   ctx.fillStyle = "#70c5ce"; //light blue
   ctx.fillRect(0, 0, cvs.width, cvs.height);
   background.draw();
   foreGround.draw();
+  bird.draw();
+  ready.draw();
+  gameOver.draw();
 }
 
 function update() {
